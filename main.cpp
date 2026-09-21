c6b25a87770d422c91fd08c38b84c445af5a4af6
Данил Смирнов <danilsmirnov960@gmail.com>
2026-09-21 10:51:15 +0300

    Программа работает

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+
+int writeToFile(string name) {
+    ofstream out;
+    out.open(name);
+
+    if (out.is_open()) {
+        srand(time(NULL));
+
+        for (int i=0; i<10; i++) {
+            
+
+            out << rand() % (10 - 1 + 1) << ' ';
+        }
+
+
+    } else {
+        return 1;
+    }
+    
+
+    out << endl;
+    out.close();
+
+    return 0;
+}
 
 int main() {
 
-    
-
+    writeToFile("file1.txt");
+    writeToFile("file2.txt");
+    writeToFile("file3.txt");
     return 0;
 }
